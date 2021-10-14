1b38f6c512a8107d641d95f91c893b06ee866254
Alena Maramygina <maratustraa@gmail.com>
2021-10-14 20:04:40 +0500

    добавлены функции getColorName и changeColor (изменяют цвет квадрата), добавлена возможность изменять рамер круга и выводить значение ползунка в %

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+'use strict';
 
+// 1.
 
+const button = document.getElementById('btn');
+const input = document.getElementById('text');
+const square = document.getElementById('square');
+let color;
 
+const getColorName = function (event) {
+
+  color = event.target.value;
+  return color;
+};
+
+const changeColor = function () {
+
+  square.style.backgroundColor = color;
+};
+
+input.addEventListener('input', getColorName);
+button.addEventListener('click', changeColor);
+
+// 2. 
+
+const btn = document.getElementById('e_btn');
+
+btn.style.display = 'none';
+
+// 3. 
+
+const range = document.getElementById('range');
+let rangeSpan = document.getElementById('range-span');
+const circle = document.getElementById('circle');
+
+range.addEventListener('input', function (event) {
+
+  rangeSpan.textContent = event.target.value + '%';
+  circle.style.height = event.target.value + '%';
+  circle.style.width = event.target.value + '%';
+});
 
 
